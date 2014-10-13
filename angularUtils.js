0a91db3110de215514f5f8832fcf3964af4c7886
Federico Rota <federico.rota01@gmail.com>
2014-10-13 11:04:31 +0200

    first commit

---
+++ angularUtils.js
@@ -0,0 +1,6 @@
+angular.module('angularUtils', ['ngSanitize'])
+	.filter('to_trusted', ['$sce', function($sce){
+            return function(text) {
+	                return $sce.trustAsHtml(text);
+	            };
+        }]);
